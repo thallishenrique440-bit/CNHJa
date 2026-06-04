0bb44d63ec8757e5553c486ad9467f282602259c
thallishenrique440-bit <Thallishenrique440@gmail.com>
2026-06-04 09:21:27 -0300

    feat(branding): rename visible app name to CNHJá

--- constants.ts
+++ constants.ts
@@ -6,7 +6,7 @@ export const TERMS_VERSION = '1.0';
 export const PRIVACY_VERSION = '1.0';
 
 export const APP_CONFIG = {
-  NAME: 'Autoescola do Brasil',
+  NAME: 'CNHJá',
   YEAR: 2026,
   SUPPORT_EMAIL: 'suporte@autoescoladobrasil.com',
 };
